c86c92df39a784419258fe430f291ebb4abecb7a
jkkjkhh <jkkjkhh@gmail.com>
2022-05-26 13:23:41 +0900

    수정 기능 추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql2/promise";
+import cors from "cors";
 
 const pool = mysql.createPool({
   host: "localhost",
@@ -14,7 +15,64 @@ const pool = mysql.createPool({
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+
 const port = 3000;
+
+app.patch("/wisesayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[wiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (wiseSayingRow === undefined) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+
+  const {
+    content = wiseSayingRow.content,
+    author = wiseSayingRow.author,
+    goodLikeCount = wiseSayingRow.goodLikeCount,
+    badLikeCount = wiseSayingRow.badLikeCount,
+  } = req.body;
+
+  await pool.query(
+    `
+    UPDATE wise_saying
+    SET content = ?,
+    author = ?, 
+    goodLikeCount = ?, 
+    badLikeCount = ?
+    WHERE id = ?
+    `,
+    [content, author, goodLikeCount, badLikeCount, id]
+  );
+
+  const [[justModifiedWiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: justModifiedWiseSayingRow,
+  });
+});
 
 app.get("/wisesayings/random", async (req, res) => {
   const [[wiseSayingRow]] = await pool.query(
